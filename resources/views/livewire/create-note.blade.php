f0afa49b4ed63d7089f73fdab8f79e9d6a496614
Jan-Lukas Müller <jan-lukas.mueller@gallerysaints.com>
2025-11-04 21:53:38 +0100

    wip: changes label of Create Note button

--- resources/views/livewire/create-note.blade.php
+++ resources/views/livewire/create-note.blade.php
@@ -112,14 +112,14 @@
                             wire:target="save"
                         >
                             <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" wire:loading.remove wire:target="save">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"/>
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
                             </svg>
                             <svg class="animate-spin w-5 h-5 mr-2" fill="none" viewBox="0 0 24 24" wire:loading wire:target="save">
                                 <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                                 <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                             </svg>
-                            <span wire:loading.remove wire:target="save">Save Note</span>
-                            <span wire:loading wire:target="save">Saving...</span>
+                            <span wire:loading.remove wire:target="save">Create Note</span>
+                            <span wire:loading wire:target="save">Creating...</span>
                         </button>
 
                         <button
